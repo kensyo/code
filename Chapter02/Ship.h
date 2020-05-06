d555d299e7fe39d1ed8d0f81c68aa90e79ca8bf2
kensyo <takitateokowa@gmail.com>
2020-05-07 02:10:49 +0900

    Add makefile for Chap02

--- Chapter02/Ship.h
+++ Chapter02/Ship.h
@@ -13,7 +13,7 @@ class Ship : public Actor
 public:
 	Ship(class Game* game);
 	void UpdateActor(float deltaTime) override;
-	void ProcessKeyboard(const uint8_t* state);
+	void ProcessKeyboard(const u_int8_t* state);
 	float GetRightSpeed() const { return mRightSpeed; }
 	float GetDownSpeed() const { return mDownSpeed; }
 private:
